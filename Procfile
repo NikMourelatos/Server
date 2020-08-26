c4c165f8015b1a4bbf29a21fc15f873f5798c2e7
NikMourelatos <nikolas@mourelatos.net>
2020-08-26 16:33:45 -0400

    heroku push

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 Service.py
+web: gunicorn wsgi:app
--- Service.py
+++ Service.py
@@ -5,7 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello world"
-
-if __name__ == '__main__':
-    app.run()
+    return "<h1>hello world iseek.</h1>"
